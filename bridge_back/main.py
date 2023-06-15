e5c8daecd4605fb40026c10e2d8710da3a040062
Carbon225 <carbon225@protonmail.com>
2023-06-15 15:49:09 +0200

    of cors

--- bridge_back/main.py
+++ bridge_back/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import healthcheck
 from .routers import lobby
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(healthcheck.router)
 app.include_router(lobby.router)
 
